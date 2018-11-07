19875761231bf066a72d39153a72ec829e31df26
Andrew Kim <drewkenjo@gmail.com>
2018-11-07 18:12:43 +0900

    refactoring

--- src/models/amp-client.js
+++ src/models/amp-client.js
@@ -1,4 +1,4 @@
-import clientAPI from 'amp-node-api-client'
+import ClientAPI from 'amp-node-api-client'
 import fs from 'fs'
 import { Wallet, getDefaultProvider } from 'ethers'
 
@@ -6,7 +6,7 @@ let keys = JSON.parse(fs.readFileSync("keys", "utf8"))
 let provider = getDefaultProvider('rinkeby')
 let wallet = new Wallet(keys.AMP2, provider)
 
-const client = new clientAPI(wallet)
+const client = new ClientAPI(wallet)
 
 export default client
 
