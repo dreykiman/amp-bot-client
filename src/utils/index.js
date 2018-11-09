7625a1cf4ca98ae581b82199b671ecc319e37c7b
Andrew Kim <drewkenjo@gmail.com>
2018-11-09 12:09:32 +0900

    handle the case when sigma==0 for gaussian approximation

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,7 @@
 import { utils } from 'ethers'
 
 export const gauss = (xx, mu, sig) => {
+  if (sig==0) return 1
 //    return ((Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random()) - 3) / 3;
   return Math.exp(-Math.pow(xx-mu,2)/2/sig/sig)/Math.sqrt(2*Math.PI*sig*sig)
 }
