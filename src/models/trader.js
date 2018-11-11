24aa82769f7a326340f07807dbdc2b703dd0f1f7
Andrew Kim <drewkenjo@gmail.com>
2018-11-12 04:23:05 +0900

    progressive flow

--- src/models/trader.js
+++ src/models/trader.js
@@ -1,3 +1,4 @@
+import { utils } from 'ethers'
 import { orderbook } from 'amp-node-api-client'
 import client from './amp-client'
 import * as binance from './binance'
@@ -65,6 +66,7 @@ const getNewOrdersForPair = tok => {
       orders.push({
         amount: 0.01*yy/gauss(price.ave, price.ave, price.dev)/xx,
         price: xx,
+        pricepoint: getPricePoints(xx).toString(),
         userAddress: client.wallet.address,
         exchangeAddress: "0x2768f1543ec9145cb680fc9699672c1a3226346d",
         makeFee: 0,
@@ -80,28 +82,75 @@ const getNewOrdersForPair = tok => {
 }
 
 
-const submitOrders = ords => {
-  let news = ords.map( ord => client.new_order(ord)
-//    .then( ord => `${ord.event.type} ${ord.event.payload.pairName}`)
-    .catch( msg => {
-      throw new Error(msg.toString())
-    }) )
+const sortOrders = (a, b) => {
+  let aprice = utils.bigNumberify( a.pricepoint )
+  let bprice = utils.bigNumberify( b.pricepoint )
 
-  return Promise.all(news)
+  if (aprice.lt(bprice)) return 1
+  else if (aprice.gt(bprice)) return -1
+
+  //cancel orders go first
+  if ('hash' in a) return 1
+  return -1
 }
 
 
+const submitOrder = (ordlist, ord) => {
+  ord.time = Date.now()
+  if ('hash' in ord) {
+    return client.cancel_order(ord.hash+(ordlist.length>0 ? "a": ""))
+      .then( cancelled => {
+        cancelled.time = Date.now()
+        ordlist.push(cancelled)
+        return ordlist
+      })
+  }
+
+  return client.new_order(ord)
+    .then( added => {
+      added.time = Date.now()
+      ordlist.push(added)
+      return ordlist
+    })
+
+  
+//  let news = ords.map( ord => client.new_order(ord)
+//    .then( ord => `${ord.event.type} ${ord.event.payload.pairName}`)
+//    .catch( msg => {
+//      throw new Error(msg.toString())
+//    }) )
+//  return Promise.all(news)
+}
+
+
+const prepOrders = pair => {
+  return getPricesForPair(pair)
+    .then( tok => {
+      let cancels = getCancelOrdersForPair(tok)
+//        .map(ele=>"cancel: "+Object.keys(ele).join('/'))
+      let newords = getNewOrdersForPair(tok)
+//        .map(ele=>"neword: "+Object.keys(ele).join('/'))
+      let allords = [...cancels, ...newords]
+
+      allords.sort( sortOrders )
+
+      return allords.reduce( (tot, ord) => {
+        tot = tot.then( ordlist => submitOrder(ordlist, ord).catch( msg => { throw {msg: msg, list: ordlist, order: ord} } ) )
+        return tot
+      }, Promise.resolve([]))
+//      .then(ordlist => 
+//        ordlist.map(ord=>ord.pricepoint)
+//      )
+
+//        .map(ele=>`${ele.pricepoint} ${ele.pairName}`)
+    }).catch(msg => {
+      return {err: msg, pair: pair}
+    })
+}
+ 
 export const populate = _ => {
-  let allorders = client.pairs()
-    .map( pair => getPricesForPair(pair)
-      .then( tok => {
-        let cancels = getCancelOrdersForPair(tok)
-        let newords = getNewOrdersForPair(tok)
-        let allords = [...cancels, ...newords]
-        allords.sort( (a,b) => {
-        })
-      }).catch(msg => {return {err: msg.toString(), pair: pair}})
-    )
+  let allorders = client.pairs().slice(0,1)
+    .map(prepOrders)
 
   return Promise.all(allorders)
 }
