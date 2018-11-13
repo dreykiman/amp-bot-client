7e161e995b24142b46712ed1559f82572f23e3fa
Andrew Kim <drewkenjo@gmail.com>
2018-11-13 14:00:32 +0900

    changed precision from 1e6 to 1e9

--- src/models/binance.js
+++ src/models/binance.js
@@ -28,7 +28,7 @@ const getPriceRange = data => {
   let dev = data.reduce( calcStandardDeviation, {total: 0, dev: 0, ave: ave, amount: 0} )
   dev = Math.sqrt( dev.dev/(dev.amount-1) )
 
-  return {dev: dev*1000, ave: ave*1000}
+  return {dev: dev, ave: ave}
 }
 
 export const getPrice = (token) => {
