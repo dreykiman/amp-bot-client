f624226d1ca3f183ab90e2e820b692b1bc88ef99
Andrew Kim <drewkenjo@gmail.com>
2018-11-13 16:19:09 +0900

    added subscription promise

--- src/app.js
+++ src/app.js
@@ -11,13 +11,16 @@ app.use('/api', router)
 
 client.start()
   .then( _ => {
-    client.pairs().slice(0.3)
+    let subscriptions = client.pairs().slice(0.3)
       .map( pair => client.subscribe(pair.baseTokenAddress, pair.quoteTokenAddress) )
+    return Promise.all(subscriptions)
   }).then( _ => {
     console.log(client.my_orders())
   }).then( _ => {
     app.listen(3000, () => {
       console.log('App listening on port 3000')
     })
+  }).catch( msg => {
+    console.log({err: msg.toString(), msg: msg})
   })
 
