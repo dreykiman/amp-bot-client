7eb61f0ea72fe384a4da1181eb28be4538abb25c
Andrew Kim <drewkenjo@gmail.com>
2018-11-14 13:54:27 +0900

    change 3000 port to 5000

--- src/app.js
+++ src/app.js
@@ -17,8 +17,8 @@ client.start()
   }).then( _ => {
     console.log(client.my_orders())
   }).then( _ => {
-    app.listen(3000, () => {
-      console.log('App listening on port 3000')
+    app.listen(5000, () => {
+      console.log('App listening on port 5000')
     })
   }).catch( msg => {
     console.log({err: msg.toString(), msg: msg})
