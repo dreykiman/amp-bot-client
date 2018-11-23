29a0d2bf55de76b4a7d8dd9667cdf464ac004ceb
Andrey <dreykiman@gmail.com>
2018-11-23 05:19:51 +0000

    added api endpoint to binance and poloniex trade endpoints

--- src/www.js
+++ src/www.js
@@ -6,14 +6,26 @@ app.use(express.static('public'))
 
 app.get('/api/pairs', (req, res) =>  {
   return rp('http://ampapi:8081/pairs', {json: true})
-    .then( data => data.data.filter(ele => ele.quoteTokenSymbol === 'WETH' ) )
     .then( data => {
-      data.forEach( ele => {
+      data.data.forEach( ele => {
         ele.pairName = `${ele.baseTokenSymbol}/${ele.quoteTokenSymbol}`
       })
-      res.send(data)
+      res.send(data.data)
     })
 })
+
+
+app.get('/api/trades/poloniex', (req, res) =>  {
+  rp('https://poloniex.com/public?command=returnTradeHistory&currencyPair='+pair, { json: true })
+    .then(res.status(200).json(data.data))
+})
+
+
+app.get('/api/trades/binance', (req, res) =>  {
+  return rp('https://api.binance.com/api/v1/trades?symbol='+pair, { json: true })
+    .then(res.status(200).json(data.data))
+})
+
 
 app.listen(3000, () => console.log('App listening on port 3000') )
 
