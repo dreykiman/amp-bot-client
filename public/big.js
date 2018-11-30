0ed0b8d243bb2e02804957300991dfb37eb7a3b6
Andrey <dreykiman@gmail.com>
2018-11-30 06:20:51 +0000

    added populate front end

---
+++ public/big.js
@@ -0,0 +1,1 @@
+/home/kenjo/big.js
--- public/utils.js
+++ public/utils.js
@@ -1,20 +1,22 @@
-function reversePrice(pricepoint) {
-  let pricePrecisionMultiplier = 1e9
-  let priceMultiplier = ethers.utils.bigNumberify('1000000000') //1e9
+function reversePrice(pricepoint, quoteDec) {
+  let precisionMultiplier = 1e9
+  let priceMultiplier = ethers.utils.bigNumberify(10).pow(18)
+  let quoteMultiplier = ethers.utils.bigNumberify(10).pow(quoteDec)
+  pricepoint = ethers.utils.bigNumberify(pricepoint)
+  pricepoint = pricepoint.mul(precisionMultiplier).div(priceMultiplier).div(quoteMultiplier)
 
-  let price = ethers.utils.bigNumberify(pricepoint)
-    .mul(ethers.utils.bigNumberify(pricePrecisionMultiplier))
-    .div(priceMultiplier)
-  return price.toString()/pricePrecisionMultiplier
+  let price = pricepoint.toString()/precisionMultiplier
+//console.log(pricepoint+" "+price)
+  return price
 }
 
-function reverseAmount(amount) {
-  let amountPrecisionMultiplier = 1e6
-  let amountMultiplier = ethers.utils.bigNumberify('1000000000000000000') //1e18
-
+function reverseAmount(amount, baseDec) {
+  let precisionMultiplier = 1e9
+  let baseMultiplier = ethers.utils.bigNumberify(10).pow(baseDec)
   amount = ethers.utils.bigNumberify(amount)
-    .mul(ethers.utils.bigNumberify(amountPrecisionMultiplier))
-    .div(amountMultiplier)
-  return amount.toString()/amountPrecisionMultiplier
+  amount = amount.mul(precisionMultiplier).div(baseMultiplier)
+console.log(baseDec)
+
+  return amount.toString()/precisionMultiplier
 }
 
