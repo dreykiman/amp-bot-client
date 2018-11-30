40741d2058a94d64d116780f82a34c474c473766
Andrey <dreykiman@gmail.com>
2018-11-30 09:02:57 +0000

    change account id

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,7 @@ import keys from '../../keys.json'
 import Trader from './trader'
 
 let provider = getDefaultProvider('rinkeby')
-let wallet = new Wallet(keys.AMP2, provider)
+let wallet = new Wallet(keys.AMPmaker, provider)
 
 const client = new ClientAPI(wallet)
 const trader = Trader(client)
